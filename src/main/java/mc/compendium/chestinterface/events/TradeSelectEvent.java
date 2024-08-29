3f5fbf51314e3ddb65fcdef00a507d1fcd569c76
onedevman <enderfight95@gmail.com>
2024-08-29 18:55:14 +0200

    update

--- src/main/java/mc/compendium/chestinterface/events/TradeSelectEvent.java
+++ src/main/java/mc/compendium/chestinterface/events/TradeSelectEvent.java
@@ -5,12 +5,12 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.Event;
 import org.bukkit.inventory.MerchantInventory;
 
-public class TradeSelectEvent extends TradeInterfaceEvent {
-    public TradeSelectEvent(Event bukkitEvent, HumanEntity entity, MerchantInventory inventory, TradeInterface menu) {
+public class TradeSelectEvent extends TradeInterfaceEvent<org.bukkit.event.inventory.TradeSelectEvent> {
+    public TradeSelectEvent(org.bukkit.event.inventory.TradeSelectEvent bukkitEvent, HumanEntity entity, MerchantInventory inventory, TradeInterface menu) {
         this(bukkitEvent, entity, inventory, menu, true);
     }
 
-    public TradeSelectEvent(Event bukkitEvent, HumanEntity entity, MerchantInventory inventory, TradeInterface menu, boolean cancellable) {
+    public TradeSelectEvent(org.bukkit.event.inventory.TradeSelectEvent bukkitEvent, HumanEntity entity, MerchantInventory inventory, TradeInterface menu, boolean cancellable) {
         super(bukkitEvent, entity, inventory, menu, cancellable);
     }
 }
