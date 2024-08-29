0836af4113568159891b37d883cde4981be9b078
onedevman <enderfight95@gmail.com>
2024-08-29 17:33:48 +0200

    update

--- src/main/java/mc/compendium/chestinterface/components/configurations/ChestMenuConfig.java
+++ src/main/java/mc/compendium/chestinterface/components/configurations/ChestMenuConfig.java
@@ -2,7 +2,7 @@ package mc.compendium.chestinterface.components.configurations;
 
 public class ChestMenuConfig extends BasicResizeableMenuConfig {
 
-    public ChestMenuConfig(String name, int rows, boolean silent) {
+    public ChestMenuConfig(String name, boolean silent, int rows) {
         super(name, silent, rows);
     }
 
